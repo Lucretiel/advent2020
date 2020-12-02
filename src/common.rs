9284debf39f6560887f61128cd1d82cd8f9ba78f
Nathan West <Lucretiel@gmail.com>
2020-12-02 04:15:28 -0500

    Add context to parse_items_iter

--- src/common.rs
+++ src/common.rs
@@ -1,23 +1,46 @@
 //! Common functionality that different puzzles may need
 
-use std::{iter::FromIterator, str::FromStr};
+use std::{error::Error, iter::FromIterator, str::FromStr};
 
+use anyhow::Context;
 /// Even more generic version of `parse_items`; takes an iterator of &str for
 /// parsing.
-pub fn parse_items_iter<'a, T: FromStr, C: FromIterator<T>>(
-    input: impl IntoIterator<Item = &'a str>,
-) -> Result<C, T::Err> {
-    input.into_iter().map(str::parse).collect()
+pub fn parse_items_iter<'a, I, T, C>(input: I) -> anyhow::Result<C>
+where
+    I: IntoIterator<Item = &'a str>,
+    T: FromStr,
+    T::Err: Send + Sync + Error + 'static,
+    C: FromIterator<T>,
+{
+    input
+        .into_iter()
+        .enumerate()
+        .map(|(i, value)| {
+            value
+                .parse()
+                .with_context(|| format!("Failed to parse item index {}", i))
+        })
+        .collect()
 }
 
 /// Advent of Code commonly gives input as a uniform list of newline or
-/// whitespace separated items. This function is generic over any parsable
-/// input item T, and any collection C which is FromIterator<T>.
-pub fn parse_items<T: FromStr, C: FromIterator<T>>(input: &str) -> Result<C, T::Err> {
+/// whitespace separated items. This parses such a list by splitting on
+/// whitespace and then parsing each individual element. This function is
+/// generic over any parsable input item T, and any collection C which is
+/// FromIterator<T>.
+pub fn parse_items<T, C>(input: &str) -> anyhow::Result<C>
+where
+    T: FromStr,
+    T::Err: Send + Sync + Error + 'static,
+    C: FromIterator<T>,
+{
     parse_items_iter(input.split_whitespace())
 }
 
+/// Helper trait for converting from `bool` to `Option`.
 pub trait BoolExt: Sized {
+    /// If the bool is true, return the result of `func`, wrapped in `Some`;
+    /// otherwise return `None`.
     fn then<T, F: FnOnce() -> T>(self, func: F) -> Option<T>;
 
     fn then_some<T>(self, value: T) -> Option<T> {
