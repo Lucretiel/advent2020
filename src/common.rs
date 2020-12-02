87571afb46828bfc6c76bcf4c3a375259aa32c84
Nathan West <Lucretiel@gmail.com>
2020-12-02 03:12:58 -0500

    Add parse_items_iter

--- src/common.rs
+++ src/common.rs
@@ -2,12 +2,17 @@
 
 use std::{iter::FromIterator, str::FromStr};
 
+/// Even more generic version of `parse_items`; takes an iterator of &str for
+/// parsing.
+pub fn parse_items_iter<'a, T: FromStr, C: FromIterator<T>>(
+    input: impl IntoIterator<Item = &'a str>,
+) -> Result<C, T::Err> {
+    input.into_iter().map(str::parse).collect()
+}
+
 /// Advent of Code commonly gives input as a uniform list of newline or
 /// whitespace separated items. This function is generic over any parsable
 /// input item T, and any collection C which is FromIterator<T>.
 pub fn parse_items<T: FromStr, C: FromIterator<T>>(input: &str) -> Result<C, T::Err> {
-    input
-        .split_whitespace()
-        .map(|value| value.parse())
-        .collect()
+    parse_items_iter(input.split_whitespace())
 }
