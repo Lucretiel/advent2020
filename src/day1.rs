92ad49707913737e42e30c4691896139b76d12e9
Nathan West <Lucretiel@gmail.com>
2020-12-02 21:31:27 -0500

    Reimplement day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,26 +1,24 @@
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 
 use anyhow::Context;
 
 use crate::common::parse_items;
 
-fn solve_recursive(values: &HashSet<i64>, target: i64, depth: u32) -> Option<i64> {
+fn solve_recursive(values: &BTreeSet<i64>, min: i64, target: i64, depth: u32) -> Option<i64> {
     match depth {
         0 => None,
         1 => values.get(&target).copied(),
-        depth => values
-            .iter()
-            .filter(|&&value| value < target)
-            .find_map(|&value| {
-                solve_recursive(values, target - value, depth - 1).map(|solution| solution * value)
-            }),
+        depth => values.range(min..target).copied().find_map(|value| {
+            solve_recursive(values, value + 1, target - value, depth - 1)
+                .map(|solution| value * solution)
+        }),
     }
 }
 
 fn solve(input: &str, depth: u32) -> anyhow::Result<i64> {
-    let values: HashSet<i64> = parse_items(input)?;
+    let values: BTreeSet<i64> = parse_items(input)?;
 
-    solve_recursive(&values, 2020, depth).context("The problem has no solution!")
+    solve_recursive(&values, 0, 2020, depth).context("The problem has no solution!")
 }
 
 pub fn part1(input: &str) -> anyhow::Result<i64> {
